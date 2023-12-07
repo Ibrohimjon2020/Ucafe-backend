532c32f98dcdb1d90b7d55b4014ab9f9622ec245
Asilbek <asilbekdavid@gmail.com>
2023-12-07 10:01:36 +0500

    add description

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Expense extends Model
 {
     use HasFactory;
-    protected $fillable = ['day','price','text'];
+    protected $fillable = ['day','price','decription'];
 
     protected $casts = [
         'day'=>'date'
